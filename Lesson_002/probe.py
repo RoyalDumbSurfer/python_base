dc90cfb9d5b42d0aa64f6734cb8c452ac621c052
Dean <comradodean@gmail.com>
2020-05-15 11:18:14 +0800

    hurko code

---
+++ Lesson_002/probe.py
@@ -0,0 +1,13 @@
+sample_dict = {
+    "foo": ["john", "peter", "mark"],
+    "bar": [1, 2, 3]
+}
+
+foo = sample_dict["foo"]
+foo.append("sam")
+
+print(f'{sample_dict["foo"]}')
+
+foo = ['trump']
+print(f'{sample_dict["foo"]}')
+
