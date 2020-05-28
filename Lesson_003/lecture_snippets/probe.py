fab4013c282727402aa30cea07cb360c46410c61
Dean <comradodean@gmail.com>
2020-05-28 20:34:09 +0800

    где 42, сука?

---
+++ Lesson_003/lecture_snippets/probe.py
@@ -0,0 +1,8 @@
+while True:
+    user_input = input('введите 42 >>')
+    result = int(user_input)
+    if result == 42:
+        print('oke')
+        break
+    else:
+        print('где 42, сука???')
