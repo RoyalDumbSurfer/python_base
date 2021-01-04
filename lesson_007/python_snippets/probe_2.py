2e65595bfa9fa00192fcefcc87059536f401516d
Dean <comradodean@gmail.com>
2021-01-04 22:36:51 +0800

    Фаня живет

--- lesson_007/python_snippets/probe_2.py
+++ lesson_007/python_snippets/probe_2.py
@@ -62,6 +62,11 @@ class Man:
         self.fullness -= 15
         self.happyness += 20
 
+    def cat_shopping(self):
+        cprint('{} закупился едой для Фани!'.format(self.name))
+        self.house.cat_food += 20
+        self.house.money -= 15
+
     def into_the_house(self, house):
         self.house = house
         self.happyness += 10
@@ -77,6 +82,8 @@ class Man:
             self.eat()
         if self.house.food <= 30:
             self.shopping()
+        if self.house.cat_food <= 20:
+            self.cat_shopping()
         if self.house.money < 20:
             self.work()
         if dice == 1:
@@ -98,7 +105,7 @@ class Cat:
         self.weight = 5
 
     def __str__(self):
-        return 'Я кошка - {}, счастья - {}, сытость - {}, вес - {}'.format(
+        return 'Я - кошка {}, счастья - {}, сытость - {}, вес - {}'.format(
             self.name, self.happyness, self.fullness, self.weight)
 
     def cat_into_the_house(self, house):
@@ -107,6 +114,38 @@ class Cat:
         self.fullness -= 2
         self.weight -= 0.5
         cprint('Кошка {} заселилась на виллу!!!'.format(self.name), color='red')
+
+    def drat_oboi(self):
+        cprint('{} дерет обои!'.format(self.name))
+        self.happyness += 5
+        self.fullness -= 1
+        self.weight -= 1
+
+    def cat_eat(self):
+        cprint('{} поела!'.format(self.name))
+        self.house.cat_food -= 5
+        self.fullness += 15
+        self.weight += 3
+
+    def cat_sleep(self):
+        cprint('{} дрыхнет...'.format(self.name))
+        self.weight += 5
+        self.happyness += 3
+
+    def livin_the_life(self):
+        dice = randint(1, 3)
+        if self.fullness <= 15:
+            self.cat_eat()
+        if self.happyness <= 20:
+            self.drat_oboi()
+        if self.weight <= 2:
+            self.cat_eat()
+        if dice == 1:
+            self.cat_eat()
+        elif dice == 2:
+            self.drat_oboi()
+        else:
+            self.cat_sleep()
 
 
 class House:
@@ -117,29 +156,40 @@ class House:
         self.cat_food = 50
 
     def __str__(self):
-        return 'В доме еды - {}, денег - {}'.format(self.food, self.money)
+        return 'В доме еды - {}, еды для кошки - {}, денег - {}'.format(self.food, self.cat_food, self.money)
 
 
 citizens = [
-    Man(name='Тедд'),
-    Man(name='Билл'),
     Man(name='Бенджи'),
-    Man(name='Бивис'),
-    Man(name='Валера'),
-    Man(name='Батхед'),
+]
+
+animals = [
+    Cat(name='Фаня')
 ]
 
 my_sweet_villa = House()
+
+for animal in animals:
+    animal.cat_into_the_house(house=my_sweet_villa)
+
 for citizen in citizens:
     citizen.into_the_house(house=my_sweet_villa)
 
+for animal in animals:
+    cprint(animal, color='cyan')
+
 for citizen in citizens:
     cprint(citizen, color='yellow')
-cprint(my_sweet_villa, color='yellow')
 
-for day in range(1, 366):
+cprint(my_sweet_villa, color='white')
+
+for day in range(1, 22):
+    if animal.happyness <= 0:
+        cprint('{} в печали'.format(animal.name), color='red')
     if citizen.happyness <= 0:
         cprint('{} грустный'.format(citizen.name), color='red')
+    if animal.fullness <= 0:
+        cprint('{} орет хочет жрать'.format(animal.name), color='red')
     if citizen.fullness <= 0:
         cprint('{} умер от голода...'.format(citizen.name), color='red')
         break
@@ -149,11 +199,17 @@ for day in range(1, 366):
     if citizen.sixpaks <= 35:
         cprint('{} умер от истощения...'.format(citizen.name), color='red')
         break
+    if animal.weight <= 1:
+        cprint('{} ушла в другой дом'.format(animal.name), color='red')
 
     print('===============================DAY {} =============================='.format(day))
     for citizen in citizens:
         citizen.livin_the_dream()
+    for animal in animals:
+        animal.livin_the_life()
     print('===============================END of THE DAY {}=============================='.format(day))
+    for animal in animals:
+        cprint(animal, color='grey')
     for citizen in citizens:
         cprint(citizen, color='grey')
     cprint(my_sweet_villa, color='grey')
