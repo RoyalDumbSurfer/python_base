52ce97b7d2c0acc2c5b73e060bbce04674e1481b
RoyalDumbSurfer <comradodean@gmail.com>
2021-08-24 22:41:59 +0800

    add the cat

--- lesson_008/01_family.py
+++ lesson_008/01_family.py
@@ -10,9 +10,10 @@ class House:
         self.money = 100
         self.food = 50
         self.dust = 0
+        self.cat_food = 50
 
     def __str__(self):
-        return 'В доме еды - {}, денег - {}, пыли - {}'.format(self.food, self.money, self.dust)
+        return 'В доме еды - {}, еды для Фани - {}, денег - {}, пыли - {}'.format(self.food, self.cat_food, self.money, self.dust)
 
 
 class Husband:
@@ -56,8 +57,12 @@ class Husband:
         self.fullness -= 20
         self.total_surf_sessions += 1
 
+    def pet_the_cat(self):
+        self.happyness += 5
+        cprint('{} играл с Фаней'.format(self.name))
+
     def act(self):
-        dice = randint(1, 4)
+        dice = randint(1, 5)
         if self.fullness <= 10:
             self.eat()
         if self.house.money < 30:
@@ -70,6 +75,8 @@ class Husband:
             self.work()
         elif dice == 3:
             self.gaming()
+        elif dice == 4:
+            self.pet_the_cat()
         else:
             self.surfing()
 
@@ -109,6 +116,17 @@ class Wife:
             cprint('Нэт дэнэг на продукты ;(', color='red')
             self.happyness -= 20
 
+    def cat_food_shopping(self):
+        if self.house.money >= 10:
+            cprint('{} сходила за едой для Фани'.format(self.name, color='green'))
+            self.fullness -= 10
+            self.house.cat_food += 10
+            self.house.money -= 10
+            self.total_money_spend += 10
+        else:
+            cprint('Нэт дэнэг на еду для Фани ;(', color='red')
+            self.happyness -= 20
+
     def buy_all_day_spa(self):
         if self.house.money >= 350:
             cprint('{} сходила в SPA'.format(self.name, color='green'))
@@ -126,8 +144,12 @@ class Wife:
         self.fullness -= 10
         self.house.dust -= 30
 
+    def pet_the_cat(self):
+        self.happyness += 5
+        cprint('{} играла с Фаней'.format(self.name))
+
     def act(self):
-        dice = randint(1, 4)
+        dice = randint(1, 6)
         if self.fullness <= 10:
             self.eat()
         if self.house.dust >= 70:
@@ -142,13 +164,62 @@ class Wife:
             self.shopping()
         elif dice == 3:
             self.clean_house()
+        elif dice == 4:
+            self.pet_the_cat()
+        elif dice == 5:
+            self.cat_food_shopping()
         else:
             self.buy_all_day_spa()
+
+
+class Cat:
+
+    def __init__(self, name):
+        self.house = home
+        self.name = name
+        self.fullness = 100
+        self.total_cat_food_eaten = 0
+
+    def __str__(self):
+        return 'Я - {}, моя сытость - {}'.format(self.name, self.fullness)
+
+    def eat(self):
+        if self.fullness < 40:
+            if self.house.cat_food >= 10:
+                cprint('{} поела'.format(self.name), color='green')
+                self.fullness += 40
+                self.house.cat_food -= 10
+                self.total_cat_food_eaten += 10
+            else:
+                cprint('В доме нэт кошачьей еды', color='red')
+        else:
+            cprint('Кот - {} не голоден'.format(self.name), color='green')
+
+    def sleep(self):
+        cprint('{} поспала'.format(self.name))
+        self.fullness -= 10
+
+    def soil(self):
+        cprint('{} драла обои'.format(self.name))
+        self.fullness -= 10
+        self.house.dust += 5
+
+    def act(self):
+        dice = randint(1, 3)
+        if self.fullness <= 20:
+            self.eat()
+        if dice == 1:
+            self.eat()
+        elif dice == 2:
+            self.soil()
+        else:
+            self.sleep()
 
 
 home = House()
 Dean = Husband(name='Дин')
 Olusha = Wife(name='Олюша')
+Fanya = Cat(name='Фаня')
 
 for day in range(1, 365):
     if home.dust >= 90:
@@ -162,12 +233,16 @@ for day in range(1, 365):
         cprint('{} очень голоден'.format(Dean.name), color='red')
     if Olusha.fullness < 0:
         cprint('{} очень голодна'.format(Olusha.name), color='red')
+    if Fanya.fullness < 0:
+        cprint('{} очень голодна'.format(Fanya.name), color='red')
     cprint('================== День {} =================='.format(day), color='red')
     home.dust += 5
     Dean.act()
     Olusha.act()
+    Fanya.act()
     cprint(Dean, color='cyan')
     cprint(Olusha, color='cyan')
+    cprint(Fanya, color='cyan')
     cprint(home, color='cyan')
 
 cprint('Всего денег было заработанно {}'.format(Dean.total_money_earned), color='green')
@@ -177,29 +252,10 @@ else:
     cprint('Всего денег было потраченно {}'.format(Olusha.total_money_spend), color='green')
 cprint('Всего Олюша съела еды {}'.format(Olusha.total_food_eaten), color='green')
 cprint('Всего Дин съел еды {}'.format(Dean.total_food_eaten), color='green')
+cprint('Всего Фаня съела еды {}'.format(Fanya.total_cat_food_eaten), color='green')
 cprint('Всего Олюша провела сессий в SPA {}'.format(Olusha.total_spa_bought), color='green')
 cprint('Всего Дин откатал серф-сессий {}'.format(Dean.total_surf_sessions), color='green')
 
-# class Cat:
-#
-#     def __init__(self):
-#         pass
-#
-#     def act(self):
-#         pass
-#
-#     def eat(self):
-#         pass
-#
-#     def sleep(self):
-#         pass
-#
-#     def soil(self):
-#         pass
-#
-#
-#
-#
 # class Child:
 #
 #     def __init__(self):
