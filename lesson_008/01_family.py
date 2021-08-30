ef9944fe33dbe3271d1360d6fbfbddb93cc5f361
RoyalDumbSurfer <comradodean@gmail.com>
2021-08-30 16:51:47 +0800

    add the child

--- lesson_008/01_family.py
+++ lesson_008/01_family.py
@@ -11,9 +11,11 @@ class House:
         self.food = 50
         self.dust = 0
         self.cat_food = 50
+        self.child_food = 100
 
     def __str__(self):
-        return 'В доме еды - {}, еды для Фани - {}, денег - {}, пыли - {}'.format(self.food, self.cat_food, self.money, self.dust)
+        return 'В доме еды - {}, еды для Ани - {}, еды для Фани - {}, денег - {}, пыли - {}' \
+            .format(self.food, self.child_food, self.cat_food, self.money, self.dust)
 
 
 class Husband:
@@ -120,11 +122,22 @@ class Wife:
         if self.house.money >= 10:
             cprint('{} сходила за едой для Фани'.format(self.name, color='green'))
             self.fullness -= 10
-            self.house.cat_food += 10
+            self.house.cat_food += 20
             self.house.money -= 10
             self.total_money_spend += 10
         else:
             cprint('Нэт дэнэг на еду для Фани ;(', color='red')
+            self.happyness -= 20
+
+    def child_food_shopping(self):
+        if self.house.money >= 20:
+            cprint('{} сходила за едой для Ани'.format(self.name, color='green'))
+            self.fullness -= 10
+            self.house.child_food += 100
+            self.house.money -= 100
+            self.total_money_spend += 100
+        else:
+            cprint('Нэт дэнэг на еду для Ани ;(', color='red')
             self.happyness -= 20
 
     def buy_all_day_spa(self):
@@ -139,6 +152,11 @@ class Wife:
             cprint('Нэт дэнэг на SPA;(', color='red')
             self.happyness -= 20
 
+    def snorkeling(self):
+        cprint('{} плавала в Океане'.format(self.name, color='green'))
+        self.fullness -= 10
+        self.happyness += 50
+
     def clean_house(self):
         cprint('{} убралась в доме'.format(self.name, color='green'))
         self.fullness -= 10
@@ -149,7 +167,9 @@ class Wife:
         cprint('{} играла с Фаней'.format(self.name))
 
     def act(self):
-        dice = randint(1, 6)
+        dice = randint(1, 8)
+        if self.house.child_food <= 20:
+            self.child_food_shopping()
         if self.fullness <= 10:
             self.eat()
         if self.house.dust >= 70:
@@ -168,8 +188,49 @@ class Wife:
             self.pet_the_cat()
         elif dice == 5:
             self.cat_food_shopping()
+        elif dice == 6:
+            self.snorkeling()
+        elif dice == 7:
+            self.child_food_shopping()
         else:
             self.buy_all_day_spa()
+
+
+class Child:
+    def __init__(self, name):
+        self.name = name
+        self.house = home
+        self.happiness = 100
+        self.fullness = 50
+        self.total_food_eaten = 0
+
+    def __str__(self):
+        return 'Я - {}, счастья - {}, сытость - {}'.format(self.name, self.happiness, self.fullness)
+
+    def sleep(self):
+        cprint('{} - поспала'.format(self.name), color='green')
+        self.fullness -= 10
+
+    def eat(self):
+        if self.fullness <= 30:
+            if self.house.child_food >= 10:
+                cprint('{} - поела'.format(self.name), color='green')
+                self.fullness += 10
+                self.house.child_food -= 10
+                self.total_food_eaten += 10
+            else:
+                cprint('Нэт еды для {}'.format(self.name), color='red')
+        else:
+            cprint('{} не хочет есть'.format(self.name))
+
+    def act(self):
+        dice = randint(1, 3)
+        if dice == 1:
+            self.eat()
+        elif dice == 2:
+            self.sleep()
+        else:
+            self.eat()
 
 
 class Cat:
@@ -220,6 +281,7 @@ home = House()
 Dean = Husband(name='Дин')
 Olusha = Wife(name='Олюша')
 Fanya = Cat(name='Фаня')
+Anna = Child(name='Анна')
 
 for day in range(1, 365):
     if home.dust >= 90:
@@ -229,19 +291,23 @@ for day in range(1, 365):
         cprint('{} в депрессии'.format(Dean.name), color='red')
     if Olusha.happyness < 10:
         cprint('{} в депрессии'.format(Olusha.name), color='red')
-    if Dean.fullness < 0:
+    if Dean.fullness <= 0:
         cprint('{} очень голоден'.format(Dean.name), color='red')
-    if Olusha.fullness < 0:
+    if Olusha.fullness <= 0:
         cprint('{} очень голодна'.format(Olusha.name), color='red')
-    if Fanya.fullness < 0:
+    if Anna.fullness <= 0:
+        cprint('{} очень голодна'.format(Anna.name), color='red')
+    if Fanya.fullness <= 0:
         cprint('{} очень голодна'.format(Fanya.name), color='red')
     cprint('================== День {} =================='.format(day), color='red')
     home.dust += 5
     Dean.act()
     Olusha.act()
+    Anna.act()
     Fanya.act()
     cprint(Dean, color='cyan')
     cprint(Olusha, color='cyan')
+    cprint(Anna, color='cyan')
     cprint(Fanya, color='cyan')
     cprint(home, color='cyan')
 
@@ -252,45 +318,7 @@ else:
     cprint('Всего денег было потраченно {}'.format(Olusha.total_money_spend), color='green')
 cprint('Всего Олюша съела еды {}'.format(Olusha.total_food_eaten), color='green')
 cprint('Всего Дин съел еды {}'.format(Dean.total_food_eaten), color='green')
+cprint('Всего Аня съела еды {}'.format(Anna.total_food_eaten), color='green')
 cprint('Всего Фаня съела еды {}'.format(Fanya.total_cat_food_eaten), color='green')
 cprint('Всего Олюша провела сессий в SPA {}'.format(Olusha.total_spa_bought), color='green')
 cprint('Всего Дин откатал серф-сессий {}'.format(Dean.total_surf_sessions), color='green')
-
-# class Child:
-#
-#     def __init__(self):
-#         pass
-#
-#     def __str__(self):
-#         return super().__str__()
-#
-#     def act(self):
-#         pass
-#
-#     def eat(self):
-#         pass
-#
-#     def sleep(self):
-#         pass
-#
-#
-#
-#
-#
-# home = House()
-# serge = Husband(name='Сережа')
-# masha = Wife(name='Маша')
-# kolya = Child(name='Коля')
-# murzik = Cat(name='Мурзик')
-#
-# for day in range(365):
-#     cprint('================== День {} =================='.format(day), color='red')
-#     serge.act()
-#     masha.act()
-#     kolya.act()
-#     murzik.act()
-#     cprint(serge, color='cyan')
-#     cprint(masha, color='cyan')
-#     cprint(kolya, color='cyan')
-#     cprint(murzik, color='cyan')
-#
