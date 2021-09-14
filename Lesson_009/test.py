40f9d18d5e238c1347247cb3c6db41604f8f8031
RoyalDumbSurfer <comradodean@gmail.com>
2021-09-14 19:49:32 +0800

    add ignoring SSl certificate

--- Lesson_009/test.py
+++ Lesson_009/test.py
@@ -1,5 +1,5 @@
-from bs4 import BeautifulSoup
 import urllib.request, urllib.parse, urllib.error
+from bs4 import BeautifulSoup
 import ssl
 
 # ignore ssl certificate errors
@@ -7,7 +7,7 @@ ctx = ssl.create_default_context()
 ctx.check_hostname = False
 ctx.verify_mode = ssl.CERT_NONE
 
-url = input('Url: ')
+url = input('Enter - ')
 html = urllib.request.urlopen(url, context=ctx).read()
 soup = BeautifulSoup(html, 'html.parser')
 
